0ba0f492009b321cce0d48631ef4a13e61e779a7
Aecio <professoraecio@gmail.com>
2023-09-04 20:06:07 -0300

    finalizar projeto
    finalizar projeto

--- NumberPicker/app/src/main/java/com/professoraecio/numberpicker/MainActivity.java
+++ NumberPicker/app/src/main/java/com/professoraecio/numberpicker/MainActivity.java
@@ -3,12 +3,33 @@ package com.professoraecio.numberpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewIdade;
+    NumberPicker numberPickerIdade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setupComponents();
+    }
+
+    private void setupComponents(){
+        textViewIdade = findViewById(R.id.textViewIdade);
+        numberPickerIdade = findViewById(R.id.numberPickerIdade);
+
+        numberPickerIdade.setMinValue(0);
+        numberPickerIdade.setMaxValue(100);
+
+        numberPickerIdade.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                textViewIdade.setText("" + i1);
+            }
+        });
     }
 }
