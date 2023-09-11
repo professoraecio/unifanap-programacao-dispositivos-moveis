569657c84258c9eae01baaa562a1cc4c2eab73c6
professoraecio <professoraecio@gmail.com>
2023-09-11 20:14:55 -0300

    Conclusão projeto
    Conclusão projeto

--- Alert-Dialog-Exemplo/app/src/main/java/com/professoraecio/alertdialogexemplo/MainActivity.java
+++ Alert-Dialog-Exemplo/app/src/main/java/com/professoraecio/alertdialogexemplo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.professoraecio.alertdialogexemplo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void perguntar(View view){
+        // Criar a caixa de diálogo (Alert Dialog)
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        // Definir o título que vai na caixa
+        alertDialog.setTitle("Atenção!");
+        // Definir o texto que vai na caixa
+        alertDialog.setMessage("Deseja realmente finalizar o cadastro?");
+        // Configurar botão sim
+        alertDialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d("debug-mode","Usuário clicou em sim...");
+            }
+        });
+        // Configurar botão cancelar
+        alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d("debug-mode","Usuário clicou em cancelar...");
+            }
+        });
+        // Criar a janela efetivamente
+        AlertDialog janela = alertDialog.create();
+        // Mostrar a janela
+        janela.show();
+
+    }
+
 }
