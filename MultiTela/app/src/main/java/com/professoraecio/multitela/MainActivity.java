97a43d92268f9f4fa27f346cca5aa9f89892fb17
professoraecio <professoraecio@gmail.com>
2023-09-11 21:48:06 -0300

    Projeto app multi-tela
    Projeto app multi-tela

---
+++ MultiTela/app/src/main/java/com/professoraecio/multitela/MainActivity.java
@@ -0,0 +1,23 @@
+package com.professoraecio.multitela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void irSegundaActivity(View view){
+        Intent intent = new Intent(this , SegundaActivity.class);
+        startActivity(intent);
+    }
+
+}
