3bc95ac7ce7ebd0a0d4f5d4e8972d2a7b87c79a8
Aecio <professoraecio@gmail.com>
2023-10-16 21:35:22 -0300

    CRUD Inicial API REST
    CRUD Inicial API REST

--- rest/agenda/app.js
+++ rest/agenda/app.js
@@ -4,6 +4,13 @@ const URL_BASE = '/api.agenda';
 const express = require('express');
 // Instanciar o express
 const app = express();
+// Instanciar o depurador morgan (debug no código)
+const morgan = require('morgan');
+app.use(morgan('dev'));
+// Instanciar o body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
 
 // Corpo do código fonte o projeto
 
@@ -17,11 +24,26 @@ app.use((req,res,next) =>{
 });
 */
 
+// Mapear a pasta routes com suas rotas
+// específicas nos arquivos (antes da URL padrão)
+
+const contatoDao = require('./routes/contatoDao');
+app.use('/api.agenda/contato',contatoDao);
+
+
 app.use(URL_BASE,(req,res,next) =>{
     res.status(200).send({
-        resposta : 'Servidor respondeu na URL BASE!'
+        resposta : 'Servidor respondeu na URL BASE! Oba'
     });
 });
+
+// Tratamento de rota não encontrada
+app.use((req,res,next) =>{
+    const erro = new Error('Rota não encontrada');
+    erro.status = 404;
+    next(erro);
+});
+
 
 
 // Fim corpo do código fonte o projeto
